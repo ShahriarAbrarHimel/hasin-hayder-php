9ebab20fa79a4da0bde00fa9a8b11a58798484ec
ShahriyarAbrarHimel <himelshahriyar7@gmail.com>
2021-05-02 01:40:49 +0600

    php

---
+++ পিএইচপির প্রাথমিক বিষয়সমূহ/পিএইচপিতে আউটপুট প্রিন্ট করা নিয়ে বিস্তারিত.php
@@ -0,0 +1,16 @@
+<?php
+$name="email";
+var_dump($name);
+$age=12;
+var_dump($age);
+$nill= null;
+var_dump($nill);
+$mp="";
+var_dump($mp);
+$hname1="shahriyar";
+$hname2="abrar";
+var_dump($hname1,$hname2);
+$result= true;
+var_dump($result);
+$name="earth";
+echo "We're living on {$name}";
