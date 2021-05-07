95976a14d93f573a2aa6e2b151d07e0addf8b7ef
ShahriyarAbrarHimel <himelshahriyar7@gmail.com>
2021-05-08 01:08:28 +0600

    done

--- পিএইচপির প্রাথমিক বিষয়সমূহ/পিএইচপিতে আউটপুট প্রিন্ট করা নিয়ে বিস্তারিত.php
+++ পিএইচপির প্রাথমিক বিষয়সমূহ/পিএইচপিতে আউটপুট প্রিন্ট করা নিয়ে বিস্তারিত.php
@@ -13,4 +13,27 @@ var_dump($hname1,$hname2);
 $result= true;
 var_dump($result);
 $name="earth";
-echo "We're living on {$name}";
+$uppername=strtoupper($name);
+echo "We're living on {$uppername}";
+echo "\n";
+printf("we 're living on %s",$name);
+echo "\n";
+$fname="shahriyar";
+$lname="abrar";
+printf("his name is %s %s",$fname,$lname);
+echo "\n";
+printf("his %s name is %s %s",'Full',$fname,$lname);
+echo"\n";
+printf("his %s %s %s $fname $lname %s","full","name","is","Himel");
+echo"
+my name is 
+{$fname} {$lname} Himel\n
+How are you??";
+echo "\n";
+echo "Total summary oh this code";
+echo"\n";
+$planet1="Amar bari";
+$planet2="Shosur bari";
+echo"i have two home on of these is ".$planet1."and another home is ".$planet2."\n";
+echo "i have two home on of these is {$planet1} and another home is {$planet2}\n";
+printf("i have two home on of these is %s and another home is %s\n",$planet1,$planet2);
